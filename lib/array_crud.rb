5379bd19f8a673728f593553af36d64bc8aa8047
Kate Summerill <kaitlynsummerill@gmail.com>
2019-08-16 00:37:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  ["kitten", "dog", "cow", "horse"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["kitten", "dog", "cow", "horse"]
+  array << "moose"
 end
 
 def add_element_to_start_of_array(array, element)
