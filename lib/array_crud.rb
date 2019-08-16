e3763542d6ad83c8b129c886338eb70ebde00ab2
Kate Summerill <kaitlynsummerill@gmail.com>
2019-08-16 00:38:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["kitten", "dog", "cow", "horse"]
-  array << "moose"
+  element << "moose"
 end
 
 def add_element_to_start_of_array(array, element)
